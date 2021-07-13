ad25de4ef1567b30ef7c4b3ce2d187c789e4291b
Lili-B <lili.bourgeois@epitech.eu>
2021-07-13 16:52:58 +0200

    [MOD] add gestion of number when checking code (not float, double, etc..)

--- src/instructionlist/checkInstructions.cpp
+++ src/instructionlist/checkInstructions.cpp
@@ -12,9 +12,52 @@
 
 #include "AbstractVM.hpp"
 
+size_t GetTypeSize(std::string value)
+{
+    if(value.find("int8") != std::string::npos) {
+        return 4;
+    } else if (value.find("int16") != std::string::npos) {
+        return 5;
+    } else if (value.find("int32") != std::string::npos) {
+        return 5;
+    } else if (value.find("float") != std::string::npos) {
+        return 5;
+    } else if (value.find("double") != std::string::npos) {
+        return 6;
+    } else if (value.find("bigdecimal") != std::string::npos) {
+        return 10;
+    }
+    return 0;
+}
+
+bool CheckTheNb(std::string line)
+{
+    double tryConvert;
+    size_t i = 0;
+    int dotCount = 0;
+
+    while (i < line.length())
+    {
+        if (line[i] == '.' && dotCount == 0 && i != 0) {
+            dotCount += 1;
+        } else if (line[i] == '.' && (dotCount != 0 || i == 0)) {
+            std::cout << "hello\n";
+            return false;
+        } else {
+            tryConvert = std::stoi(line.substr(0, 1));
+            std::cout << "try convert : " << tryConvert << "\n";
+            if (tryConvert == 0)
+                return false;
+            line.erase(0, 1);
+        }
+    }
+    return true;
+}
+
 bool CheckSyntacticalError(std::string line, avm::eInstruction enumInstruction, const char *strInstruction[])
 {
     size_t instructionSize = strlen(strInstruction[enumInstruction]); //TODO
+    size_t typeSize = 0;
 
     if (enumInstruction > 3) {
         std::cout << "line" << line.length() << "\n";
@@ -24,7 +67,16 @@ bool CheckSyntacticalError(std::string line, avm::eInstruction enumInstruction, 
             return false;
         }
     } else if (0 <= enumInstruction && enumInstruction <= 3) {
-       return true;
+        line.erase(0, instructionSize + 1);
+        typeSize = GetTypeSize(line);
+        if (typeSize == 0) {
+           return false;
+        }
+        line.erase(0, typeSize + 1);
+        /*line.pop_back();
+        if (CheckTheNb(line) == 0)
+            return false;*/
+        std::cout << "line = '" << line << "'\n";
     }
     return true;
 }
@@ -44,7 +96,7 @@ bool FindInstruction(std::string codeAsm, const char *strInstruction[])
                 std::cout << "enuminstruction :" << enumInstruction << "\n";
                 std::cout << "instruction :" << strInstruction[enumInstruction] << "\n\n\n\n";
                 if (CheckSyntacticalError(line, enumInstruction, strInstruction) == false) {
-                    std::cout << "syntactical error \n";
+                    std::cout << "syntactical error\n";
                     return false;
                 }
                 enumInstruction = avm::eInstruction(18);
