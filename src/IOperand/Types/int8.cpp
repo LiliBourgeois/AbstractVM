0e611cd3c4dca9bcbbd99d6cd15191249bdf8203
pmatheo <matheo.pons@epitech.eu>
2021-07-13 09:27:04 +0200

    [FIX] IOperand pure virtual error [RM] Operand class

--- src/IOperand/Types/int8.cpp
+++ src/IOperand/Types/int8.cpp
@@ -5,6 +5,11 @@
 ** int8.cpp
 */
 
+#include <iostream>
+#include <string>
+#include <limits>   
+
+#include "Factory.hpp"
 #include "int8.hpp"
 
 avm::myInt8::myInt8(const std::string &value)
@@ -16,4 +21,74 @@ avm::myInt8::myInt8(const std::string &value)
 
 avm::myInt8::~myInt8()
 {
+}
+
+avm::IOperand *avm::myInt8::operator+(const IOperand &other) const
+{
+    std::string result;
+    avm::IOperand *newOperand;
+    avm::Factory fct;
+
+    int8_t thisValue = static_cast<int8_t>(std::stod(*this->value));
+    int8_t otherValue = static_cast<int8_t>(std::stod(other.toString()));
+    if ((std::numeric_limits<int8_t>::max() - (std::numeric_limits<int8_t>::min() + thisValue)) < otherValue) {
+        std::cerr << "myInt8::operator+ error: overflow or underflow\n";
+        return (NULL);
+    }
+
+    result = std::to_string(thisValue + otherValue);
+    newOperand = fct.createOperand(this->getType(), result);
+    return (newOperand);
+}
+
+avm::IOperand *avm::myInt8::operator-(const IOperand &other) const
+{
+    std::string result;
+    avm::IOperand *newOperand;
+    avm::Factory fct;
+
+    double thisValue = static_cast<double>(std::stod(*this->value));
+    double otherValue = static_cast<double>(std::stod(other.toString()));
+    result = std::to_string(thisValue - otherValue);
+    newOperand = fct.createOperand(this->getType(), result);
+    return (newOperand);
+}
+
+avm::IOperand *avm::myInt8::operator*(const IOperand &other) const
+{
+    std::string result;
+    avm::IOperand *newOperand;
+    avm::Factory fct;
+
+    double thisValue = static_cast<double>(std::stod(*this->value));
+    double otherValue = static_cast<double>(std::stod(other.toString()));
+    result = std::to_string(thisValue * otherValue);
+    newOperand = fct.createOperand(this->getType(), result);
+    return (newOperand);
+}
+
+avm::IOperand *avm::myInt8::operator/(const IOperand &other) const
+{
+    std::string result;
+    avm::IOperand *newOperand;
+    avm::Factory fct;
+
+    double thisValue = static_cast<double>(std::stod(*this->value));
+    double otherValue = static_cast<double>(std::stod(other.toString()));
+    result = std::to_string(thisValue / otherValue);
+    newOperand = fct.createOperand(this->getType(), result);
+    return (newOperand);
+}
+
+avm::IOperand *avm::myInt8::operator%(const IOperand &other) const
+{
+    std::string result;
+    avm::IOperand *newOperand;
+    avm::Factory fct;
+
+    uint64_t thisValue = static_cast<int>(std::stod(*this->value));
+    uint64_t otherValue = static_cast<int>(std::stod(other.toString()));
+    result = std::to_string(thisValue % otherValue);
+    newOperand = fct.createOperand(this->getType(), result);
+    return (newOperand);
 }
